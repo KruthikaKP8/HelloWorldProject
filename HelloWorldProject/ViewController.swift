06cfb1de5182d558a59365b7993ec32aa5479aee
KruthikaKP8 <60217322+KruthikaKP8@users.noreply.github.com>
2020-01-24 11:51:52 +0530

    Add files via upload

---
+++ HelloWorldProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HelloWorldProject
+//
+//  Created by Kruthika KP on 23/01/20.
+//  Copyright © 2020 Kruthika KP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
